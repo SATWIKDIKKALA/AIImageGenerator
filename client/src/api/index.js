2b31e875ef4453a1cc10ccb07aaa37b666efbe2e
SATWIKDIKKALA <43822762+SATWIKDIKKALA@users.noreply.github.com>
2025-03-02 21:03:11 -0500

    Initial commit

---
+++ client/src/api/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://image-generator-y1ch.onrender.com",
+});
+
+export const GetPosts = async () => await API.get("/post/");
+export const CreatePost = async (data) => await API.post("/post/", data);
+export const GenerateAIImage = async (data) =>
+  await API.post("/generateImage/", data);
